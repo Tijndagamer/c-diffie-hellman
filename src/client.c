416abc8147286415760a06cddbd4628f71ef9eca
Martijn <mrtijn@riseup.net>
2018-01-21 15:51:25 +0100

    Fix segfault after host resolving error and print error messages to stderr
    And also fix some return statements.

--- src/client.c
+++ src/client.c
@@ -6,6 +6,7 @@
  */
 
 #include "client.h"
+#include "util.h"
 #include "diffiehellman.h"
 
 int client(char *port, char *host)
@@ -20,19 +21,21 @@ int client(char *port, char *host)
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    if (getaddrinfo(host, port, &hints, &res) != 0)
-        printf("Error resolving host information\n");
+    if (getaddrinfo(host, port, &hints, &res) != 0) {
+        printerr("Error resolving host information\n");
+        return -1;
+    }
 
     sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     if (sockfd < 0) {
-        printf("Error opening socket");
-        exit(1);
+        printerr("Error opening socket");
+        return -1;
     }
 
     printf("Connecting to %s:%s...", host, port);
     if (connect(sockfd, res->ai_addr, res->ai_addrlen) < 0) {
-        printf("Could not connect to %s", host);
-        exit(1);
+        printerr("Could not connect to %s", host);
+        return -1;
     }
     printf("done.\n");
 
@@ -44,14 +47,18 @@ int client(char *port, char *host)
     int s_sec;
     int p_sec = 4;
     s_sec = diffiehellman_c(sockfd, p, g, p_sec);
+    if (s_sec == 0) {
+        printerr("Error exchaning keys\n");
+        return -1;
+    }
     printf("Shared secret key: %d\n", s_sec);
 
     close(sockfd);
     printf("Connection closed.\n");
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {
-    client(argv[1], argv[2]);
-    return 0;
+    return client(argv[1], argv[2]);
 }
